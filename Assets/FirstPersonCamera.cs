c48f383cc4dca211df7db653ace34202a10514f6
Georgianna Lin <georgiannalin@gmail.com>
2019-11-06 01:28:57 -0500

    Room + cursor + double keyboard

--- Assets/FirstPersonCamera.cs
+++ Assets/FirstPersonCamera.cs
@@ -5,17 +5,29 @@ using System.Collections;
 public class FirstPersonCamera : MonoBehaviour
 {
 
-    public float speedH = 2.0f;
-    public float speedV = 2.0f;
-
-    private float yaw = 0.0f;
-    private float pitch = 0.0f;
+    public float speed = 2.0f;
+    public float speedf = 30f;
 
     void Update()
     {
-        yaw += speedH * Input.GetAxis("Mouse X");
-        pitch -= speedV * Input.GetAxis("Mouse Y");
+        //Camera Rotate
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Rotate(new Vector3(-speedf * Time.deltaTime, 0, 0), Space.Self);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(new Vector3(0, -speedf * Time.deltaTime, 0), Space.Self);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.Rotate(new Vector3(speedf * Time.deltaTime, 0, 0), Space.Self);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(new Vector3(0, speedf * Time.deltaTime, 0), Space.Self);
+        }
+         new WaitForSeconds(1);
 
-        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
     }
 }
